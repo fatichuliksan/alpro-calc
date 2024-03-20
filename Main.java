8708f32c7d59eaa16b89e4f3b36a80e6bdd01503
Fatichul Iksan <fatichul.iksan@gmail.com>
2024-03-21 05:20:22 +0700

    fix: implement basic operand and fixing

--- Main.java
+++ Main.java
@@ -14,23 +14,29 @@ public class Main {
         System.out.println("Pilih operasi (1=Penjumlahan, 2=Pengurangan, 3=Perkalian, 4=Pembagian): ");
         int choice = scanner.nextInt();
 
-        // switch(choice) {
-        // case 1:
-        // System.out.println("Hasil: " + Calculator.addition(a, b));
-        // break;
-        // case 2:
-        // System.out.println("Hasil: " + Calculator.subtraction(a, b));
-        // break;
-        // case 3:
-        // System.out.println("Hasil: " + Calculator.multiplication(a, b));
-        // break;
-        // case 4:
-        // System.out.println("Hasil: " + Calculator.division(a, b));
-        // break;
-        // default:
-        // System.out.println("Pilihan tidak valid.");
-        // break;
-        // }
+        double result = 0;
+
+        Calculator calculator = new Calculator();
+
+        switch (choice) {
+            case 1:
+                result = calculator.add(a, b);
+                break;
+            case 2:
+                result = calculator.subtract(a, b);
+                break;
+            // case 3:
+            // System.out.println("Hasil: " + Calculator.multiplication(a, b));
+            // break;
+            // case 4:
+            // System.out.println("Hasil: " + Calculator.division(a, b));
+            // break;
+            default:
+                System.out.println("Pilihan tidak valid.");
+                break;
+        }
+
+        System.out.println("Hasil: " + result);
 
         scanner.close();
     }
